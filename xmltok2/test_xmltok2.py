f3744fec9b6977c03b04d4dbee4a35708a4cab3c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-07-31 20:24:08 +0300

    xmltok2: Add tests.

---
+++ xmltok2/test_xmltok2.py
@@ -0,0 +1,25 @@
+import xmltok2
+
+expected = [
+['PI', 'xml', None, None],
+['ATTR', '', 'version', '1.0'],
+['START_TAG', 's', 'Envelope', None],
+['ATTR', 'xmlns', 's', 'http://schemas.xmlsoap.org/soap/envelope/'],
+['ATTR', 's', 'encodingStyle', 'http://schemas.xmlsoap.org/soap/encoding/'],
+['START_TAG', 's', 'Body', None],
+['START_TAG', 'u', 'GetConnectionTypeInfo', None],
+['ATTR', 'xmlns', 'u', 'urn:schemas-upnp-org:service:WANIPConnection:1'],
+['TEXT', 'foo bar\n  baz\n  \n', None, None],
+['END_TAG', 'u', 'GetConnectionTypeInfo', None],
+['END_TAG', 's', 'Body', None],
+['END_TAG', 's', 'Envelope', None],
+]
+
+dir = "."
+if "/" in __file__:
+    dir = __file__.rsplit("/", 1)[0]
+
+ex = iter(expected)
+for i in xmltok2.tokenize(open(dir + "/test.xml")):
+    print(i)
+    assert i == next(ex)
