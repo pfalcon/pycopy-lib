ae697ff643920a7ea43cf6d270d9ab4a36052aeb
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-08-05 13:39:27 +0300

    uaiohttpclient: Support arbitrary ports in URL.

--- uaiohttpclient/uaiohttpclient.py
+++ uaiohttpclient/uaiohttpclient.py
@@ -50,7 +50,11 @@ def request_raw(method, url):
         path = ""
     if proto != "http:":
         raise ValueError("Unsupported protocol: " + proto)
-    reader, writer = yield from asyncio.open_connection(host, 80)
+    port = 80
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+    reader, writer = yield from asyncio.open_connection(host, port)
     # Use protocol 1.0, because 1.1 always allows to use chunked transfer-encoding
     # But explicitly set Connection: close, even though this should be default for 1.0,
     # because some servers misbehave w/o it.
