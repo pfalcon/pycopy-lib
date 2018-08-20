6c22e6da1cb4e3b21d390f6bd9608e081ecb9513
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-08-20 10:19:25 +0300

    cpython-uhashlib: Release 0.1.

---
+++ cpython-uhashlib/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+
+setup(name='micropython-cpython-uhashlib',
+      version='0.1',
+      description='MicroPython module uhashlib ported to CPython',
+      long_description='This is MicroPython compatibility module, allowing applications using\nMicroPython-specific features to run on CPython.\n',
+      url='https://github.com/pfalcon/micropython-lib',
+      author='Paul Sokolovsky',
+      author_email='micropython-lib@googlegroups.com',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='micropython-lib@googlegroups.com',
+      license='Python',
+      py_modules=['uhashlib'])
