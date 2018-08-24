cd0824305225cd9b214b031e9d252a7e3a3ec718
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-08-24 13:01:48 +0300

    urequests: Handle HTTP redirects.
    By adding a wrapping loop to the existing request() method, avoid adding
    a wrapping method to avoid extra function call overhead (well, mostly
    param passing overhead).

--- urequests/urequests.py
+++ urequests/urequests.py
@@ -33,83 +33,96 @@ class Response:
 
 
 def request(method, url, data=None, json=None, headers={}, stream=None, parse_headers=True):
-    try:
-        proto, dummy, host, path = url.split("/", 3)
-    except ValueError:
-        proto, dummy, host = url.split("/", 2)
-        path = ""
-    if proto == "http:":
-        port = 80
-    elif proto == "https:":
-        import ussl
-        port = 443
-    else:
-        raise ValueError("Unsupported protocol: " + proto)
+    redir_cnt = 1
+    while True:
+        try:
+            proto, dummy, host, path = url.split("/", 3)
+        except ValueError:
+            proto, dummy, host = url.split("/", 2)
+            path = ""
+        if proto == "http:":
+            port = 80
+        elif proto == "https:":
+            import ussl
+            port = 443
+        else:
+            raise ValueError("Unsupported protocol: " + proto)
 
-    if ":" in host:
-        host, port = host.split(":", 1)
-        port = int(port)
+        if ":" in host:
+            host, port = host.split(":", 1)
+            port = int(port)
 
-    ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
-    ai = ai[0]
+        ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
+        ai = ai[0]
 
-    resp_d = None
-    if parse_headers is not False:
-        resp_d = {}
+        resp_d = None
+        if parse_headers is not False:
+            resp_d = {}
 
-    s = usocket.socket(ai[0], ai[1], ai[2])
-    try:
-        s.connect(ai[-1])
-        if proto == "https:":
-            s = ussl.wrap_socket(s, server_hostname=host)
-        s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
-        if not "Host" in headers:
-            s.write(b"Host: %s\r\n" % host)
-        # Iterate over keys to avoid tuple alloc
-        for k in headers:
-            s.write(k)
-            s.write(b": ")
-            s.write(headers[k])
-            s.write(b"\r\n")
-        if json is not None:
-            assert data is None
-            import ujson
-            data = ujson.dumps(json)
-            s.write(b"Content-Type: application/json\r\n")
-        if data:
-            s.write(b"Content-Length: %d\r\n" % len(data))
-        s.write(b"Connection: close\r\n\r\n")
-        if data:
-            s.write(data)
+        s = usocket.socket(ai[0], ai[1], ai[2])
+        try:
+            s.connect(ai[-1])
+            if proto == "https:":
+                s = ussl.wrap_socket(s, server_hostname=host)
+            s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
+            if not "Host" in headers:
+                s.write(b"Host: %s\r\n" % host)
+            # Iterate over keys to avoid tuple alloc
+            for k in headers:
+                s.write(k)
+                s.write(b": ")
+                s.write(headers[k])
+                s.write(b"\r\n")
+            if json is not None:
+                assert data is None
+                import ujson
+                data = ujson.dumps(json)
+                s.write(b"Content-Type: application/json\r\n")
+            if data:
+                s.write(b"Content-Length: %d\r\n" % len(data))
+            s.write(b"Connection: close\r\n\r\n")
+            if data:
+                s.write(data)
 
-        l = s.readline()
-        #print(l)
-        l = l.split(None, 2)
-        status = int(l[1])
-        reason = ""
-        if len(l) > 2:
-            reason = l[2].rstrip()
-        while True:
             l = s.readline()
-            if not l or l == b"\r\n":
-                break
             #print(l)
-            if l.startswith(b"Transfer-Encoding:"):
-                if b"chunked" in l:
-                    raise ValueError("Unsupported " + l)
-            elif l.startswith(b"Location:") and not 200 <= status <= 299:
-                raise NotImplementedError("Redirects not yet supported")
-            if parse_headers is False:
-                pass
-            elif parse_headers is True:
-                l = l.decode()
-                k, v = l.split(":", 1)
-                resp_d[k] = v.strip()
-            else:
-                parse_headers(l, resp_d)
-    except OSError:
-        s.close()
-        raise
+            l = l.split(None, 2)
+            status = int(l[1])
+            reason = ""
+            if len(l) > 2:
+                reason = l[2].rstrip()
+            while True:
+                l = s.readline()
+                if not l or l == b"\r\n":
+                    break
+                #print(l)
+
+                if l.startswith(b"Transfer-Encoding:"):
+                    if b"chunked" in l:
+                        raise ValueError("Unsupported " + l)
+                elif l.startswith(b"Location:") and 300 <= status <= 399:
+                    if not redir_cnt:
+                        raise ValueError("Too many redirects")
+                    redir_cnt -= 1
+                    url = l[9:].decode().strip()
+                    #print("redir to:", url)
+                    status = 300
+                    break
+
+                if parse_headers is False:
+                    pass
+                elif parse_headers is True:
+                    l = l.decode()
+                    k, v = l.split(":", 1)
+                    resp_d[k] = v.strip()
+                else:
+                    parse_headers(l, resp_d)
+        except OSError:
+            s.close()
+            raise
+
+        if status != 300:
+            break
 
     resp = Response(s)
     resp.status_code = status
