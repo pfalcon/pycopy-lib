15ffba386ee5d6bca0b8ea2ef51efb8b56ef2d78
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-09-02 22:15:19 +0300

    framebuf: Release 0.1.

---
+++ framebuf/setup.py
@@ -0,0 +1,21 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+import sdist_upip
+
+setup(name='micropython-framebuf',
+      version='0.1',
+      description='Implementation of "framebuf" module (using SDL2) for the "unix" port of MicroPython. (Experimental)',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
+      url='https://github.com/pfalcon/micropython-lib',
+      author='Paul Sokolovsky',
+      author_email='micropython-lib@googlegroups.com',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='micropython-lib@googlegroups.com',
+      license='MIT',
+      cmdclass={'sdist': sdist_upip.sdist},
+      py_modules=['framebuf'],
+      install_requires=['micropython-usdl2'])
