a1b9d265e71755beda20502f8eee97873c17576c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-09-07 20:07:10 +0300

    pprint: Add dummy indent, width, depth params (ignored).

--- pprint/pprint.py
+++ pprint/pprint.py
@@ -1,5 +1,5 @@
-def pformat(obj):
+def pformat(obj, indent=1, width=80, depth=None):
     return repr(obj)
 
-def pprint(obj, stream=None):
+def pprint(obj, stream=None, indent=1, width=80, depth=None):
     print(repr(obj), file=stream)
