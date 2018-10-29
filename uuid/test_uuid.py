8e29e7ddc859e3df3d34335b8bfe521d61b13fb1
Andrew Leech <andrew@alelec.net>
2018-10-29 16:00:07 +1100

    uuid: simplify uuid module

--- uuid/test_uuid.py
+++ uuid/test_uuid.py
@@ -5,27 +5,11 @@ u2 = uuid.uuid4()
 
 assert str(u1) != str(u2), "Two uuid4 should not match"
 
-same = {
-    uuid.UUID('{12345678-1234-5678-1234-567812345678}'),
-    uuid.UUID('12345678123456781234567812345678'),
-    uuid.UUID('12345678-1234-5678-1234-567812345678'),
-    uuid.UUID(bytes=b'\x12\x34\x56\x78'*4),
-    uuid.UUID(bytes_le=b'\x78\x56\x34\x12\x34\x12\x78\x56' +
-                  b'\x12\x34\x56\x78\x12\x34\x56\x78'),
-    uuid.UUID(fields=(0x12345678, 0x1234, 0x5678, 0x12, 0x34, 0x567812345678)),
-    uuid.UUID(int=0x12345678123456781234567812345678)
-}
+assert len(str(u1)) == len(str(u1)) == 36
 
-print(same)
+assert str(repr(u1)).startswith("<UUID")
+assert str(repr(u1)).endswith(">")
 
-assert len(same) == 1, "Alternate methods of initialising UUID should match"
-
-u3 = same.pop()
-
-assert str(u3) == '12345678-1234-5678-1234-567812345678', "str convertion should create standard uuid representation"
-
-assert u3.hex == '12345678123456781234567812345678'
-
-assert u3.__bytes__() == b'\x12\x34\x56\x78'*4
+assert len(u1.hex) == 32
 
 print("OK")
