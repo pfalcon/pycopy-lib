a5f4f3d37a03008d095f6b1a7a2cd13ce9e26801
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-10 20:43:43 +0300

    json: Convert to metadata.txt, release 3.3.3-1.

--- json/setup.py
+++ json/setup.py
@@ -1,16 +1,21 @@
-#import sys
-# Remove current dir from sys.path, otherwise distutils will peek up our
-# copy module instead of system.
-#sys.path.pop(0)
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
 from setuptools import setup
+sys.path.append("..")
+import sdist_upip
 
 setup(name='micropython-json',
-      version='0.1',
-      description='CPython json package ported to MicroPython',
-      url='https://github.com/micropython/micropython/issues/405',
+      version='3.3.3-1',
+      description='CPython json module ported to MicroPython',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
+      url='https://github.com/pfalcon/micropython-lib',
       author='CPython Developers',
-      maintainer='MicroPython Developers',
-      maintainer_email='micro-python@googlegroups.com',
+      author_email='python-dev@python.org',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='micropython-lib@googlegroups.com',
       license='Python',
-      install_requires=['micropython-re-pcre'],
-      packages=['json'])
+      cmdclass={'sdist': sdist_upip.sdist},
+      packages=['json'],
+      install_requires=['micropython-re-pcre'])
