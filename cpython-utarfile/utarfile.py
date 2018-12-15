35ac320b23da460e5f114c23ffb21a9af9a09f04
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-16 00:47:51 +0300

    cpython-utarfile: Make sure that TarFile works in streaming mode.

--- cpython-utarfile/utarfile.py
+++ cpython-utarfile/utarfile.py
@@ -1,2 +1,8 @@
-from tarfile import TarFile, TarInfo
+import micropython
+import tarfile
+from tarfile import TarInfo
 from tarfile import DIRTYPE, REGTYPE
+
+
+def TarFile(fileobj):
+    return tarfile.TarFile.open(fileobj=fileobj, mode="r|")
