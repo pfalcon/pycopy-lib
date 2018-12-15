a08eca2326265a770c280ec90463eb84d68c7f95
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-15 23:48:58 +0300

    cpython-uio: Add stream wrapper which implements MicroPython stream semantics.
    Support .write(data, offset, size) method, and allow to write utf-8 str
    data to binary streams.

--- cpython-uio/uio.py
+++ cpython-uio/uio.py
@@ -1,1 +1,35 @@
+import io
 from io import BytesIO, StringIO
+
+
+class UioStream:
+
+    def __init__(self, s, is_bin):
+        self._s = s
+        self._is_bin = is_bin
+
+    def write(self, data, off=None, sz=None):
+        if self._is_bin and isinstance(data, str):
+            data = data.encode()
+
+        if off is not None:
+            if sz is None:
+                sz = off
+                off = 0
+            data = memoryview(data)[off:off + sz]
+
+        self._s.write(data)
+
+    def __getattr__(self, attr):
+        return getattr(self._s, attr)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return self._s.__exit__(*args)
+
+
+def open(name, mode="r", *args, **kw):
+    f = io.open(name, mode, *args, **kw)
+    return UioStream(f, "b" in mode)
