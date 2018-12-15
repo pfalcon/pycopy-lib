73362f150995f1ba0871ec2f14a20425a7a36d81
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-16 00:36:13 +0300

    cpython-uzlib: Release 0.1.

---
+++ cpython-uzlib/setup.py
@@ -0,0 +1,19 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+
+setup(name='micropython-cpython-uzlib',
+      version='0.1',
+      description='MicroPython module uzlib ported to CPython',
+      long_description='This is MicroPython compatibility module, allowing applications using\nMicroPython-specific features to run on CPython.\n',
+      url='https://github.com/pfalcon/micropython-lib',
+      author='Paul Sokolovsky',
+      author_email='micropython-lib@googlegroups.com',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='micropython-lib@googlegroups.com',
+      license='Python',
+      py_modules=['uzlib'],
+      install_requires=['micropython-cpython-micropython'])
