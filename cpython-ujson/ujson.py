b94c25b2b3608eec4b258db158303d2230365dbc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-18 11:09:48 +0300

    cpython-ujson: Add workarounds for CPython 3.5 and lower re: binary data.
    CPython 3.5 and lower didn't support binary inputs to .loads() and binary
    streams to .load(). Work that around by converting to str/TextIOWrapper
    for those versions.

--- cpython-ujson/ujson.py
+++ cpython-ujson/ujson.py
@@ -1,2 +1,24 @@
 import micropython
-from json import dump, dumps, load, loads
+import json
+from json import dump, dumps
+import io
+import sys
+
+
+# Before CPython3.6. only str input was acceptet by json module
+_json_only_str = sys.version_info < (3, 6)
+
+
+def load(stream):
+    if _json_only_str:
+        # io.TextIOWrapper doesn't support text streams as input,
+        # so we should pass only binary streams to it.
+        if not isinstance(stream, (io.TextIOWrapper, io.StringIO)):
+            stream = io.TextIOWrapper(stream)
+    return json.load(stream)
+
+
+def loads(s):
+    if _json_only_str and not isinstance(s, str):
+        s = s.decode()
+    return json.loads(s)
