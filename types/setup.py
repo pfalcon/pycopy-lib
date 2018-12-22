be8ee9dcdf85dc035f10c0fae240138510e49ae6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-22 12:01:11 +0300

    types: Release 3.3.3-2.

--- types/setup.py
+++ types/setup.py
@@ -1,11 +1,20 @@
-from distutils.core import setup
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+import sdist_upip
 
 setup(name='micropython-types',
-      version='0.0.1',
+      version='3.3.3-2',
       description='CPython types module ported to MicroPython',
-      url='https://github.com/micropython/micropython/issues/405',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
+      url='https://github.com/pfalcon/micropython-lib',
       author='CPython Developers',
-      maintainer='MicroPython Developers',
-      maintainer_email='micro-python@googlegroups.com',
+      author_email='python-dev@python.org',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='micropython-lib@googlegroups.com',
       license='Python',
+      cmdclass={'sdist': sdist_upip.sdist},
       py_modules=['types'])
