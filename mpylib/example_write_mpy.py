d21065d05c2ca50aaf6e8cda5197acc8a9fc29c7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-02-14 01:36:34 +0300

    mpylib: example_write_mpy: Assemble bytecode instruction by instruction.

--- mpylib/example_write_mpy.py
+++ mpylib/example_write_mpy.py
@@ -1,4 +1,17 @@
 import mpylib
+import opcode
+
+class AttrDict:
+
+    def __init__(self, d):
+        self.d = d
+
+    def __getattr__(self, k):
+        return self.d[k]
+
+
+op = AttrDict(opcode.opmap)
+
 
 with open("testout.mpy", "wb") as f:
     mpy = mpylib.MPYOutput(f)
@@ -14,13 +27,25 @@ with open("testout.mpy", "wb") as f:
 
     co.co_lnotab = b'\x00\x00'
     co.co_cellvars = ()
-    co.co_code = b'\x1b\xc9\x00\x00\x81d\x012\x11['
+
+#    co.co_code = b'\x1b\xc9\x00\x00\x81d\x012\x11['
+
+    bc = mpylib.Bytecode()
+    bc.init_bc()
+    bc.add(op.LOAD_NAME, "print")
+    bc.add(op.LOAD_CONST_SMALL_INT, -65)
+    bc.add(op.LOAD_CONST_OBJ, "string")
+    bc.add(op.CALL_FUNCTION, 2, 0)
+    bc.add(op.POP_TOP)
+    bc.add(op.LOAD_CONST_NONE)
+    bc.add(op.RETURN_VALUE)
+    co.co_code = bc.get_bc()
+    co.co_names = bc.co_names
+    co.mpy_consts = bc.co_consts
 
     co.co_name = "<module>"
     co.co_filename = "testmpy.py"
 
-    co.co_names = ["print"]
-    co.mpy_consts = ()
     co.mpy_codeobjs = ()
 
     mpy.write_code(co)
