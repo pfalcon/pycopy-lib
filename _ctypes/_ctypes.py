ea3945a25fae481b8e76dc991fd9fdf43b8c0105
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-05-27 11:12:10 +0300

    _ctypes: Reimplementation of CPython internal _ctypes module for uPy.
    The idea is to try to reuse existing ctypes code (with patching it, as
    usual). ctypes import various stuff from _ctypes (which is in CPython
    written in C), so start to implement that.

---
+++ _ctypes/_ctypes.py
@@ -0,0 +1,72 @@
+# (c) 2019 Paul Sokolovsky, MIT license
+__version__ = "1.1.0"
+
+import struct
+import ffi
+
+
+Union, Structure, Array = 0, 0, 0
+_Pointer = 0
+
+RTLD_LOCAL, RTLD_GLOBAL = 1, 2
+
+FUNCFLAG_CDECL = 1
+FUNCFLAG_PYTHONAPI = 4
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class _SimpleCData:
+
+    def __init__(self, value=0):
+        print("!", value)
+        self.value = value
+
+    def __str__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.value)
+
+
+class CFuncPtr:
+
+    def __init__(self, addr_or_sym):
+        if isinstance(addr_or_sym, int):
+            self.addr = addr_or_sym
+        else:
+            self.addr = addr_or_sym[1]._handle.addr(addr_or_sym[0])
+        print("CFuncPtr(%r): %r" % (addr_or_sym, self.addr))
+
+    def __call__(self, *args):
+        print("*", args)
+        builtin_map = {int: "i", bytes: "P"}
+        argspec = ""
+        callargs = []
+        for a in args:
+            if isinstance(a, _SimpleCData):
+                callargs.append(a.value)
+                argspec += a._type_
+            else:
+                callargs.append(a)
+                argspec += builtin_map[type(a)]
+        f = ffi.func("i", self.addr, argspec)
+        return f(*callargs)
+
+
+def dlopen(name, mode):
+    print("dlopen(%s, %x)" % (name, mode))
+    return ffi.open(name)
+
+
+def sizeof(typ):
+    tmap = {"O": "P", "g": "q", "c": "b", "z": "P"}
+    code = typ._type_
+    return struct.calcsize(tmap.get(code, code))
+
+
+_libc = ffi.open("libc.so.6")
+_memmove_addr = _libc.addr("memmove")
+_memset_addr = _libc.addr("memset")
+
+
+_pointer_type_cache = {}
