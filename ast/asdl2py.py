18312eac3aca501acdaedd5a334cac5cc23de728
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-07 18:23:58 +0300

    ast: asdl2py.py: Generate _fields property for AST subclasses.
    Primary motivation is stable (and CPython-compatible) field order when
    dumping AST trees.

--- ast/asdl2py.py
+++ ast/asdl2py.py
@@ -4,12 +4,14 @@ import asdl
 
 t = asdl.parse(sys.argv[1])
 for dfn in t.dfns:
-    print("class %s(AST): pass" % dfn.name)
     #print("#", dfn.value)
     if isinstance(dfn.value, asdl.Sum):
+        print("class %s(AST): pass" % dfn.name)
         for typ in dfn.value.types:
-            print("class %s(%s): pass" % (typ.name, dfn.name))
+            print("class %s(%s):" % (typ.name, dfn.name))
+            print("    _fields = %s" % repr(tuple([f.name for f in typ.fields])))
     elif isinstance(dfn.value, asdl.Product):
-        pass
+        print("class %s(AST):" % dfn.name)
+        print("    _fields = %s" % repr(tuple([f.name for f in dfn.value.fields])))
     else:
         assert 0
