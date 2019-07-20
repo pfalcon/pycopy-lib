75e9985163a18c31ba31102fbbc666bf98ab6e1e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-21 00:36:43 +0300

    utokenize: Accept only valid string prefixes.
    Otherwise, CPython allows to have no space between keyword and string, e.g.
    if"".

--- utokenize/utokenize.py
+++ utokenize/utokenize.py
@@ -130,7 +130,7 @@ def tokenize(readline):
                 while l and (l[0].isalpha() or l[0].isdigit() or l.startswith("_")):
                     name += l[0]
                     l = l[1:]
-                if l.startswith('"') or l.startswith("'"):
+                if (l.startswith('"') or l.startswith("'")) and name in ("b", "r", "rb", "br", "u", "f"):
                     s, l, lineno_delta = get_str(l, readline)
                     yield TokenInfo(STRING, name + s, lineno, 0, org_l)
                     lineno += lineno_delta
