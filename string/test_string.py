b27f91b2952ccd138e30ac2ab56c383eefb3a824
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-23 01:53:42 +0300

    string: test_string: Tests for expandtabs().

--- string/test_string.py
+++ string/test_string.py
@@ -5,3 +5,9 @@ assert translate("foobar", {ord("o"): "foo", ord("b"): 32, ord("r"): None}) == "
 assert isidentifier("_foo13_1")
 assert not isidentifier("_foo13_1-")
 assert not isidentifier("1_foo13_1")
+
+# expandtabs
+assert expandtabs("\t") == '        '
+assert expandtabs("123\t") == '123     '
+assert expandtabs("12345678\t") == '12345678        '
+assert expandtabs("123\n\t") == '123\n        '
