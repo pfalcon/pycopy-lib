acd7cdf5efc54593fcf97f015476599dd086e307
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-25 08:01:17 +0300

    utokenize: Handle both lower- and upper- case number prefixes.

--- utokenize/utokenize.py
+++ utokenize/utokenize.py
@@ -106,10 +106,10 @@ def tokenize(readline):
         while l:
             if l[0].isdigit() or (l.startswith(".") and len(l) > 1 and l[1].isdigit()):
                 t = ""
-                if l.startswith("0x"):
+                if l.startswith("0x") or l.startswith("0X"):
                     t = "0x"
                     l = l[2:]
-                elif l.startswith("0o"):
+                elif l.startswith("0o") or l.startswith("0O"):
                     t = "0o"
                     l = l[2:]
                 while l and (l[0].isdigit() or l[0] == "." or (t.startswith("0x") and l[0] in "ABCDEFabcdef")):
