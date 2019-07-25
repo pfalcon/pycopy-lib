f2026babf94072b8df901611b8c92e41636316bc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-25 07:59:34 +0300

    utokenize: testdata: Testcase for strings (especially triple-quoted).

---
+++ utokenize/testdata/strings.py
@@ -0,0 +1,36 @@
+"fdfd"
+'fsdgfd'
+"12\"34"
+"12\""
+"\"34"
+"12\r\n56"
+b"foo"
+r"bar"
+u"123"
+br"2332"
+rb"321432"
+
+"""foo"""
+"""foo""" + 1
+
+"""
+foo
+"""
+
+"""\
+foo
+"""
+
+"""A minimal subset of the locale module used at interpreter startup
+(imported by the _io module), in order to reduce startup time.
+
+Don't import directly from third-party code; use the `locale` module instead!
+"""
+
+"""
+\"""
+"""
+
+"""\""""""
+
+bg="#d00" if clear == NORMAL else"#fca"
