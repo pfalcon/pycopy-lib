9e66866d5841dd10578930da947852ef41d84a9b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-30 23:06:12 +0300

    ast: testdata: Testcase for str/bytes literals.

---
+++ ast/testdata/string.py
@@ -0,0 +1,30 @@
+b"foo"
+"foo"
+"foo" "bar"
+b"foo" b"bar"
+
+r"\nfoo\s"
+rb"\nfoo\s"
+
+"a\\b\'b\"b"
+
+"a\sb"
+
+"a\x01b"
+"a\0"
+"a\00"
+"a\000"
+"a\0000"
+
+b"\x90"
+
+"""foo
+bar"""
+
+"""\
+foo
+bar
+"""
+
+"a\u007fb"
+"a\U0000007fb"
