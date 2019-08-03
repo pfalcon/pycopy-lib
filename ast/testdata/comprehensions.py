dc0e60c2f07ee21767dcd31db67b6fe94ef66f86
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-03 08:57:24 +0300

    ast: testdata: Testcase for comprehensions.

---
+++ ast/testdata/comprehensions.py
@@ -0,0 +1,19 @@
+{x: x + 1 for x in y}
+
+(x for x in foo)
+(x for x in foo if bar)
+(x for x in foo if bar if baz)
+any(x for x in foo)
+[x for x in foo]
+[(x for x in foo)]
+[x for x in foo if 2]
+
+(x for x in y for y in foo)
+(x for x in y for y in foo if y)
+(x for x in y if x for y in foo)
+(x for x in y if x for y in foo if y)
+[x for x in y if x for y in foo if y]
+
+{x for x in y}
+
+[lambda: None for x in y]
