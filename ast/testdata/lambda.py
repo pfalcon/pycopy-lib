99e93b7f90ab5718754a6af02381eafa7743a186
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-04 13:36:59 +0300

    ast: testdata: Add complex lambda expressions.
    From https://docs.python.org/3/faq/programming.html#is-it-possible-to-write-obfuscated-one-liners-in-python

---
+++ ast/testdata/lambda.py
@@ -0,0 +1,18 @@
+# From https://docs.python.org/3/faq/programming.html#is-it-possible-to-write-obfuscated-one-liners-in-python
+
+# Primes < 1000
+print(list(filter(None,map(lambda y:y*reduce(lambda x,y:x*y!=0,
+map(lambda x,y=y:y%x,range(2,int(pow(y,0.5)+1))),1),range(2,1000)))))
+
+# First 10 Fibonacci numbers
+print(list(map(lambda x,f=lambda x,f:(f(x-1,f)+f(x-2,f)) if x>1 else 1:
+f(x,f), range(10))))
+
+# Mandelbrot set
+print((lambda Ru,Ro,Iu,Io,IM,Sx,Sy:reduce(lambda x,y:x+y,map(lambda y,
+Iu=Iu,Io=Io,Ru=Ru,Ro=Ro,Sy=Sy,L=lambda yc,Iu=Iu,Io=Io,Ru=Ru,Ro=Ro,i=IM,
+Sx=Sx,Sy=Sy:reduce(lambda x,y:x+y,map(lambda x,xc=Ru,yc=yc,Ru=Ru,Ro=Ro,
+i=i,Sx=Sx,F=lambda xc,yc,x,y,k,f=lambda xc,yc,x,y,k,f:(k<=0)or (x*x+y*y
+>=4.0) or 1+f(xc,yc,x*x-y*y+xc,2.0*x*y+yc,k-1,f):f(xc,yc,x,y,k,f):chr(
+64+F(Ru+x*(Ro-Ru)/Sx,yc,0,0,i)),range(Sx))):L(Iu+y*(Io-Iu)/Sy),range(Sy
+))))(-2.1, 0.7, -1.2, 1.2, 30, 80, 24))
