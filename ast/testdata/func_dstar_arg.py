ba153e61256cfd91f79e6eff85b1ff97a3260f59
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-04 11:56:00 +0300

    ast: testdata: Testcases for various function argument types and annotations.

---
+++ ast/testdata/func_dstar_arg.py
@@ -0,0 +1,6 @@
+def foo(a, b, **c):
+    pass
+
+def foo(a, *b, **c):
+    pass
+
---
+++ ast/testdata/func_star_arg.py
@@ -0,0 +1,7 @@
+def foo(a, b, *c):
+    pass
+
+# After vararg, only kwonly's
+def merge(*iterables, key=None, reverse=False):
+    pass
+
