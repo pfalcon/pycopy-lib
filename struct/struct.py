6e11623dd1e72860a44d62bc31fd4561cb859a47
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-12 18:39:11 +0300

    struct: Initial normalization of format string to ustruct conventions.

--- struct/struct.py
+++ struct/struct.py
@@ -1,4 +1,37 @@
-from ustruct import *
+import sys
+import ustruct
+
+
+error = ValueError
+
+
+def _norm_fmt(fmt):
+    f = ""
+    if fmt.startswith("="):
+        f = "<"
+        if sys.byteorder == "big":
+            f = ">"
+    elif fmt.startswith("!"):
+        f = ">"
+    if f:
+        fmt = f + fmt[1:]
+
+    fmt = fmt.replace("?", "B")
+
+    return fmt
+
+
+def calcsize(fmt):
+    return ustruct.calcsize(_norm_fmt(fmt))
+
+
+def pack(fmt, *vals):
+    return ustruct.pack(_norm_fmt(fmt), *vals)
+
+
+def unpack(fmt, buf):
+    return ustruct.unpack(_norm_fmt(fmt), buf)
+
 
 class Struct:
 
