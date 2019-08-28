807ff692e61d54d71a8c0fdfe8ec77abf6be94a2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-28 20:22:46 +0300

    cpython-ucollections: Implement deque class extensions.

--- cpython-ucollections/ucollections.py
+++ cpython-ucollections/ucollections.py
@@ -1,1 +1,20 @@
-from collections import OrderedDict, deque, namedtuple
+import collections
+from collections import OrderedDict, namedtuple
+
+
+class deque:
+
+    def __init__(self, iterable, maxlen, flags=0):
+        assert iterable == ()
+        self.maxlen = maxlen
+        self.flags = flags
+        self.d = collections.deque(iterable, maxlen)
+
+    def append(self, x):
+        if self.flags & 1:
+            if len(self.d) == self.maxlen:
+                raise IndexError
+        self.d.append(x)
+
+    def popleft(self):
+        return self.d.popleft()
