68c32093a3d88ea7d7c8adb32d957453172791ee
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-28 08:59:21 +0300

    cpython-ure: Add compile(), match(), search().

--- cpython-ure/ure.py
+++ cpython-ure/ure.py
@@ -3,5 +3,14 @@ import re
 # In Pycopy's ure, "." matches newline by default, effectively
 # as if DOTALL flag was used.
 
+def compile(pattern, flags=0):
+    return re.compile(pattern, flags | re.DOTALL)
+
+def match(pattern, string):
+    return re.match(pattern, string, re.DOTALL)
+
+def search(pattern, string):
+    return re.search(pattern, string, re.DOTALL)
+
 def sub(pattern, repl, string, count=0, flags=0):
     return re.sub(pattern, repl, string, count, flags | re.DOTALL)
