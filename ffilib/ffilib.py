0ad5ee6666a77aeb0a8e08240f30f8fc20239b1d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-04 23:30:08 +0300

    ffilib: Update for Pycopy project.

--- ffilib/ffilib.py
+++ ffilib/ffilib.py
@@ -37,7 +37,7 @@ def libc():
     return open("libc", 6)
 
 # Find out bitness of the platform, even if long ints are not supported
-# TODO: All bitness differences should be removed from micropython-lib, and
+# TODO: All bitness differences should be removed from pycopy-lib, and
 # this snippet too.
 bitness = 1
 v = sys.maxsize
