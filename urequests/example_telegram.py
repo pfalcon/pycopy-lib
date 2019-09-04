da5fff2820a7654c5352c7187d783e02501265bc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-04 23:25:52 +0300

    urequests: example_telegram: Update for Pycopy project.

--- urequests/example_telegram.py
+++ urequests/example_telegram.py
@@ -12,6 +12,6 @@ print(r.content)
 print(r.json())
 
 # It's mandatory to close response objects as soon as you finished
-# working with them. On MicroPython platforms without full-fledged
+# working with them. On Pycopy platforms without full-fledged
 # OS, not doing so may lead to resource leaks and malfunction.
 r.close()
