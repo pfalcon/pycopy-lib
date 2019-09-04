a2490075b09d681b4615fbbab59f168ec41e5c15
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-04 23:59:41 +0300

    random: test_randrange: Update for Pycopy.

--- random/test_randrange.py
+++ random/test_randrange.py
@@ -6,7 +6,7 @@ UPPER = 100
 s = set()
 
 # This number of course depends on a particular PRNG and its default seed
-# as used by MicroPython.
+# as used by Pycopy.
 for c in range(496):
     r = randrange(UPPER)
     s.add(r)
