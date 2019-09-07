137a7722482de7c29be4c9c507198bfff1c7572b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-07 21:30:30 +0300

    operator: test_operator: Add test for methodcaller().

--- operator/test_operator.py
+++ operator/test_operator.py
@@ -1,7 +1,19 @@
 import operator
 
-class A: pass
+class A:
+
+    def method(self, *args, **kwargs):
+        return (args, kwargs)
+
+
 a = A()
+
 a.name = "foo"
 f = operator.attrgetter('name')
 assert f(a) == "foo"
+
+fm = operator.methodcaller("method")
+assert fm(a) == ((), {})
+
+fm = operator.methodcaller("method", 1, 2, 3, kw1="foo", kw2=10)
+assert fm(a) == ((1, 2, 3), {"kw1": "foo", "kw2": 10})
