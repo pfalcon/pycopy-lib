90668aad20f2a547daaa46d3c7b791d7c2cfddd9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-11 00:46:10 +0300

    opcode: Add stack_effect() function.
    Currently just uses op_stack_effect table. As not all opcodes are supported,
    has debug logging.

--- opcode/opcode/__init__.py
+++ opcode/opcode/__init__.py
@@ -1,2 +1,10 @@
 from .upyopcodes import *
 from .upyopmap import *
+
+
+def stack_effect(opcode, *args):
+    delta = op_stack_effect[opcode]
+    if delta is not None:
+        return delta
+    print(opcode, *args)
+    assert 0, opname[opcode]
