e4032b35d83981f82891e4839b8c971a2eb62813
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-09-25 13:25:57 -0700

    pprint: Implement pformat() in terms of pprint().
    pprint() allows streaming operation, so it only makes sense to treat it
    as a primary API, and implement pformat() using pprint() to uio.StringIO.

--- pprint/pprint.py
+++ pprint/pprint.py
@@ -1,5 +1,11 @@
+import uio
+
+
 def pformat(obj, indent=1, width=80, depth=None):
-    return repr(obj)
+    buf = uio.StringIO()
+    pprint(obj, buf, indent, width, depth)
+    return buf.getvalue()
+
 
 def pprint(obj, stream=None, indent=1, width=80, depth=None):
     print(repr(obj), file=stream)
