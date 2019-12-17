658001d869057820b5251168e12655adc8d17a9a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-12-17 23:46:32 +0300

    mmap: Check for mmap() call failure.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- mmap/mmap.py
+++ mmap/mmap.py
@@ -1,6 +1,7 @@
-# (c) 2018 Paul Sokolovsky. MIT license.
+# (c) 2018-2019 Paul Sokolovsky. MIT license.
 import uctypes
 import ffilib
+import os
 
 
 libc = ffilib.libc()
@@ -18,4 +19,5 @@ MAP_ANONYMOUS = 0x20
 # So far, implement just buffer interface of CPython
 def mmap(fd, sz, flags, prot, access=None, offset=0):
     ptr = mmap_(None, sz, prot, flags, fd, offset)
+    os.check_error(ptr)
     return uctypes.bytearray_at(ptr, sz)
