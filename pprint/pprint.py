c5730f47d8a4650e9e3fb279a2834b9f688c402c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-12-22 03:30:01 +0300

    pprint: Very rough attempt to implement pretty-printing for dicts.

--- pprint/pprint.py
+++ pprint/pprint.py
@@ -1,3 +1,4 @@
+import sys
 import uio
 
 
@@ -8,4 +9,19 @@ def pformat(obj, indent=1, width=80, depth=None):
 
 
 def pprint(obj, stream=None, indent=1, width=80, depth=None):
-    print(repr(obj), file=stream)
+    if stream is None:
+        stream = sys.stdout
+
+    if isinstance(obj, dict):
+        stream.write("{\n")
+        for k, v in obj.items():
+            stream.write("  ")
+            pprint(k, stream, indent, width, depth)
+            stream.write(": ")
+            pprint(v, stream, indent, width, depth)
+            stream.write(",\n")
+        stream.write("}")
+    else:
+        print(repr(obj), file=stream, end="")
+
+#    print(file=stream)
