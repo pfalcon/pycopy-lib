17b0438b8f06b94ef14e2b621e9ba04594537043
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-12-27 00:53:49 +0300

    cpython-ffi: Handle "l" and "L" type codes.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- cpython-ffi/ffi.py
+++ cpython-ffi/ffi.py
@@ -39,6 +39,8 @@ class DynMod:
         "s": ctypes.c_char_p,
         "i": ctypes.c_int,
         "I": ctypes.c_uint,
+        "l": ctypes.c_long,
+        "L": ctypes.c_ulong,
         "q": ctypes.c_longlong,
         "Q": ctypes.c_ulonglong,
         "p": ctypes.c_void_p,
