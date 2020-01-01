086314dca6f09fddfc810e566fcb4a3e9ce63e6e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-01-02 00:31:23 +0300

    ast: NodeVisitor.generic_visit: Dispatch only on AST nodes in lists.
    E.g., don't dispatch on None values which may appear there, causing crash.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ast/ast/__init__.py
+++ ast/ast/__init__.py
@@ -85,7 +85,8 @@ class NodeVisitor:
             val = getattr(node, f)
             if isinstance(val, list):
                 for v in val:
-                    self.visit(v)
+                    if isinstance(v, AST):
+                        self.visit(v)
             elif isinstance(val, AST):
                 self.visit(val)
 
