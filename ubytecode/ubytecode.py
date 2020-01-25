dffcf5c6591b8695c6866c14e722c0cd04432c8e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-01-26 00:24:40 +0200

    ubytecode: Handle 2 args for CALL_FUNCTION_VAR_KW.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ubytecode/ubytecode.py
+++ ubytecode/ubytecode.py
@@ -78,7 +78,7 @@ class Bytecode:
             arg = args[0]
         fl, extra = upyopcodes.mp_opcode_type(opcode)
 
-        if opcode == opmap["CALL_FUNCTION"]:
+        if opcode in (opmap["CALL_FUNCTION"], opmap["CALL_FUNCTION_VAR_KW"]):
             MPYWriter.write_uint(None, args[0] + (args[1] << 8), self.buf)
         elif opcode == opmap["LOAD_CONST_SMALL_INT"]:
             MPYWriter.write_int(None, arg, self.buf)
