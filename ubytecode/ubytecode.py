d8e479336ad9436495fae171983d6cac31eee57c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-02-08 10:35:09 +0700

    ubytecode: Bytecode: Rename co_consts to mpy_consts.
    While this is not important for in-memory representation, serialized .mpy
    format requires that data constant object vs code objects were grouped
    together and go one after another. So, preparing to introduce separate
    list for code objects, rename the old one to emphasize that it may
    (depending on options hopefully) store only data constants.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ubytecode/ubytecode.py
+++ ubytecode/ubytecode.py
@@ -57,7 +57,7 @@ class Bytecode:
     def __init__(self):
         self.buf = uio.BytesIO()
         self.co_names = []
-        self.co_consts = []
+        self.mpy_consts = []
         self.labels = []
         self.stk_ptr = 0
         self.stk_use = 0
@@ -84,14 +84,14 @@ class Bytecode:
         elif opcode == opmap["LOAD_CONST_SMALL_INT"]:
             MPYOutput.write_int(None, arg, self.buf)
         elif opcode == opmap["LOAD_CONST_OBJ"]:
-            MPYOutput.write_uint(None, len(self.co_consts), self.buf)
-            self.co_consts.append(arg)
+            MPYOutput.write_uint(None, len(self.mpy_consts), self.buf)
+            self.mpy_consts.append(arg)
         elif opcode in (opmap["MAKE_FUNCTION"], opmap["MAKE_FUNCTION_DEFARGS"]):
-            MPYOutput.write_uint(None, len(self.co_consts), self.buf)
-            self.co_consts.append(arg)
+            MPYOutput.write_uint(None, len(self.mpy_consts), self.buf)
+            self.mpy_consts.append(arg)
         elif opcode in (opmap["MAKE_CLOSURE"], opmap["MAKE_CLOSURE_DEFARGS"]):
-            MPYOutput.write_uint(None, len(self.co_consts), self.buf)
-            self.co_consts.append(arg)
+            MPYOutput.write_uint(None, len(self.mpy_consts), self.buf)
+            self.mpy_consts.append(arg)
             self.buf.writebin("B", args[1])
         elif opcode == opmap["RAISE_VARARGS"]:
             self.buf.writebin("B", arg)
@@ -117,7 +117,7 @@ class Bytecode:
             self.exc_stk_ptr -= 1
 
     def add_const(self, c):
-        self.co_consts.append(c)
+        self.mpy_consts.append(c)
 
     def set_flag(self, fl):
         self.co_flags |= fl
@@ -179,8 +179,8 @@ class Bytecode:
         co = CodeType()
         co.co_code = self.get_bc()
         co.co_names = self.co_names
-        co.co_consts = self.co_consts
-        co.mpy_consts = self.co_consts
+        co.co_consts = self.mpy_consts
+        co.mpy_consts = self.mpy_consts
         co.co_stacksize = co.mpy_stacksize = self.stk_use
         co.mpy_excstacksize = self.exc_stk_use
         co.co_flags = self.co_flags
