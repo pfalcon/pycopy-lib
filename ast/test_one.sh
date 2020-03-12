008500979a2268bf85b165c2acb93dcd6e506d86
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-03-13 00:26:35 +0200

    ast: test_one.sh: Increase heap size to 34M.
    As required for some sources from CPython stdlib/testsuite.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ast/test_one.sh
+++ ast/test_one.sh
@@ -1,5 +1,5 @@
 set -e
 
 python3.5 example_ast_parse_cpy.py $1 >$1.cpy
-pycopy -X heapsize=24M example_ast_parse_stream.py $1 >$1.upy
+pycopy -X heapsize=34M example_ast_parse_stream.py $1 >$1.upy
 diff -u $1.cpy $1.upy
