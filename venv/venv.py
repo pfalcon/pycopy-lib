d4d6c7b8be8ab95309c728496e7fb8c9c9f36bd2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-03-26 09:28:01 +0200

    venv: Add rudimentary help text.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- venv/venv.py
+++ venv/venv.py
@@ -2,6 +2,10 @@ import sys
 import os
 
 
+if len(sys.argv) != 2:
+    print("usage: venv ENV_DIR")
+    sys.exit(1)
+
 env = sys.argv[1]
 os.makedirs(env + "/bin")
 with open(env + "/bin/activate", "w") as f:
