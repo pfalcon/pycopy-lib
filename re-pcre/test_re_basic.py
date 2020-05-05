fbef547a258fbf7bc2e43be3bd0a8508f7f04437
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-05-06 01:52:10 +0300

    re-pcre: test_re_basic: Better group the tests by function.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- re-pcre/test_re_basic.py
+++ re-pcre/test_re_basic.py
@@ -1,8 +1,14 @@
 import re
 
+assert re.escape(r"1243*&[]_dsfAd") == r"1243\*\&\[\]_dsfAd"
+
+# search
+
 m = re.search(r"a+", "caaab")
 assert m.group(0) == "aaa"
 assert m.group() == "aaa"
+
+# match
 
 m = re.match(r"(?ms)foo.*\Z", "foo\nbar")
 assert m.group(0) == "foo\nbar"
@@ -11,19 +17,21 @@ assert re.match(r"a+", "caaab") is None
 m = re.match(r"a+", "aaaab")
 assert m.group(0) == "aaaa"
 
+m = re.match(r"(\d+)\.(\d+)", "24.1632")
+assert m.groups() == ('24', '1632')
+assert m.group(2, 1) == ('1632', '24')
+
+# sub
+
 assert re.sub("a", "z", "caaab") == "czzzb"
 assert re.sub("a+", "z", "caaab") == "czb"
 assert re.sub("a", "z", "caaab", 1) == "czaab"
 assert re.sub("a", "z", "caaab", 2) == "czzab"
 assert re.sub("a", "z", "caaab", 10) == "czzzb"
-
+assert re.sub(r"[ :/?&]", "_", "http://foo.ua/bar/?a=1&b=baz/") == "http___foo.ua_bar__a=1_b=baz_"
 assert re.sub("a", lambda m: m.group(0) * 2, "caaab") == "caaaaaab"
 
-m = re.match(r"(\d+)\.(\d+)", "24.1632")
-assert m.groups() == ('24', '1632')
-assert m.group(2, 1) == ('1632', '24')
-
-assert re.escape(r"1243*&[]_dsfAd") == r"1243\*\&\[\]_dsfAd"
+# split
 
 assert re.split('x*', 'foo') == ['foo']
 assert re.split("(?m)^$", "foo\n\nbar\n") == ["foo\n\nbar\n"]
@@ -32,8 +40,6 @@ assert re.split('(\W+)', 'Words, words, words.') == ['Words', ', ', 'words', ', 
 assert re.split('\W+', 'Words, words, words.', 1) == ['Words', 'words, words.']
 assert re.split('[a-f]+', '0a3B9', flags=re.IGNORECASE) == ['0', '3', '9']
 assert re.split('(\W+)', '...words, words...') == ['', '...', 'words', ', ', 'words', '...', '']
-
-assert re.sub(r"[ :/?&]", "_", "http://foo.ua/bar/?a=1&b=baz/") == "http___foo.ua_bar__a=1_b=baz_"
 
 # findall
 
