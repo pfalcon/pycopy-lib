9c6ab6c56c1294f8cfe193692593cf856be3bb4f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-06-05 23:20:24 +0300

    _libc: Recast as a module which exports generic libc functions, like strlen().
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- _libc/_libc.py
+++ _libc/_libc.py
@@ -1,34 +1,6 @@
-import ffi
-import sys
+import ffilib
 
 
-_h = None
+libc = ffilib.libc()
 
-names = ('libc.so', 'libc.so.0', 'libc.so.6', 'libc.dylib')
-
-def get():
-    global _h
-    if _h:
-        return _h
-    err = None
-    for n in names:
-        try:
-            _h = ffi.open(n)
-            return _h
-        except OSError as e:
-            err = e
-    raise err
-
-
-def set_names(n):
-    global names
-    names = n
-
-# Find out bitness of the platform, even if long ints are not supported
-# TODO: All bitness differences should be removed from pycopy-lib, and
-# this snippet too.
-bitness = 1
-v = sys.maxsize
-while v:
-    bitness += 1
-    v >>= 1
+strlen = libc.func("L", "strlen", "P")
