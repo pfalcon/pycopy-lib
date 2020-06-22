fa13a568dd55a8ea4cc3dbf47623f1997b63fda5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-06-23 00:13:02 +0300

    ssl: Update for ussl.SSLContext.
    ussl module no longer provides global wrap_socket() function. Instead,
    SSLContext.wrap_socket() method should be used.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ssl/ssl.py
+++ ssl/ssl.py
@@ -7,22 +7,49 @@ for sym in "CERT_NONE", "CERT_OPTIONAL", "CERT_REQUIRED":
         globals()[sym] = object()
 
 
-def wrap_socket(sock, keyfile=None, certfile=None, server_side=False,
-                cert_reqs=CERT_NONE, *, ca_certs=None, server_hostname=None):
-    # TODO: More arguments accepted by CPython could also be handled here.
-    # That would allow us to accept ca_certs as a positional argument, which
-    # we should.
+PROTOCOL_TLS = 2
+
+
+class SSLContext:
+
+    def __init__(self, protocol=PROTOCOL_TLS):
+        self._ctx = _ussl.SSLContext(protocol)
+
+    def wrap_socket(
+        self, sock, server_side=False, do_handshake_on_connect=True,
+        suppress_ragged_eofs=True, server_hostname=None, session=None
+    ):
+        return self._ctx.wrap_socket(
+            sock,
+            server_hostname=server_hostname,
+            do_handshake=do_handshake_on_connect,
+        )
+
+
+def wrap_socket(
+    sock, keyfile=None, certfile=None, server_side=False, cert_reqs=CERT_NONE,
+    ssl_version=PROTOCOL_TLS, ca_certs=None, do_handshake_on_connect=True,
+    suppress_ragged_eofs=True, ciphers=None
+):
+    ctx = SSLContext(ssl_version)
+
     kw = {}
+    key = cert = None
     if keyfile is not None:
-        kw["keyfile"] = keyfile
+        with open(keyfile, "rb") as f:
+            key = f.read()
     if certfile is not None:
-        kw["certfile"] = certfile
+        with open(certfile, "rb") as f:
+            cert = f.read()
+    if key or cert:
+        ctx.set_cert_key(cert, key)
+
+    if ca_certs is not None:
+        raise NotImplementedError
+
+    kw = {}
     if server_side is not False:
         kw["server_side"] = server_side
     if cert_reqs is not CERT_NONE:
         kw["cert_reqs"] = cert_reqs
-    if ca_certs is not None:
-        kw["ca_certs"] = ca_certs
-    if server_hostname is not None:
-        kw["server_hostname"] = server_hostname
-    return _ussl.wrap_socket(sock, **kw)
+    return ctx.wrap_socket(sock, **kw)
