b6552f5b1669dccd9b8d141834fc19d229a3f1d5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-07-25 08:28:57 +0300

    uastunparse: Implement Subscript.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- uastunparse/uastunparse.py
+++ uastunparse/uastunparse.py
@@ -99,6 +99,12 @@ class ASTUnparse(ast.NodeVisitor):
             self.f.write(" ")
         self.visit(node.operand)
 
+    def visit_Subscript(self, node):
+        self.visit(node.value)
+        self.f.write("[")
+        self.visit(node.slice)
+        self.f.write("]")
+
     def visit_Attribute(self, node):
         self.visit(node.value)
         self.f.write(".")
