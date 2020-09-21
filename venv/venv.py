2bcdc7e3aeb369c10dc21556ca888439b05eaca4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-09-21 10:48:44 +0300

    venv: Switch to PYCOPYPATH envvar.
    Following Pycopy itself switching to it.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- venv/venv.py
+++ venv/venv.py
@@ -10,5 +10,5 @@ env = sys.argv[1]
 os.makedirs(env + "/bin")
 with open(env + "/bin/activate", "w") as f:
     f.write("""\
-export MICROPYPATH=%s
+export PYCOPYPATH=%s
 """ % os.path.abspath(env + "/lib"))
