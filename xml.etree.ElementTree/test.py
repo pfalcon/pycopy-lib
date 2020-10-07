2d41caea74762d06ed4f26f611026c641bce5379
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-10-07 12:49:19 +0300

    xml.etree.ElementTree: test: Extend to check parsing of .text and .tail .

--- xml.etree.ElementTree/test.py
+++ xml.etree.ElementTree/test.py
@@ -2,7 +2,7 @@ import io
 import xml.etree.ElementTree as ET
 
 
-DATA = '<html attr1="val1" attr2="val2"><body>foo</body></html>'
+DATA = '<html attr1="val1" attr2="val2"><body>foo</body><tag1><tag2>bar</tag2>tail2</tag1>tail1</html>'
 
 
 root = ET.fromstring(DATA)
@@ -17,4 +17,23 @@ print(root, root.tag, root.attrib, root[0].tag)
 
 assert root.tag == "html"
 assert root.attrib == {'attr1': 'val1', 'attr2': 'val2'}
-assert len(root) == 1
+assert root.text is None
+assert root.tail is None
+assert len(root) == 2
+
+t = root[0]
+assert t.tag == "body"
+assert t.text == "foo"
+assert t.tail is None
+
+t = root[1]
+assert t.tag == "tag1"
+assert t.text is None
+assert t.tail == "tail1"
+assert len(t) == 1
+
+t = t[0]
+assert t.tag == "tag2"
+assert t.text == "bar"
+assert t.tail == "tail2"
+assert len(t) == 0
