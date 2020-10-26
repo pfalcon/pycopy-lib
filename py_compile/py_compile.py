95d8c9b24832f756033bd3213a44d86977d4c97f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-10-26 19:01:42 +0300

    py_compile: Update for pycopy-cross.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- py_compile/py_compile.py
+++ py_compile/py_compile.py
@@ -1,7 +1,7 @@
 import os
 
 def compile(file, cfile=None):
-    cmdline = "mpy-cross %s" % file
+    cmdline = "pycopy-cross %s" % file
     # For now, just hardcode -mcache-lookup-bc, as that's default for
     # Unix port.
     cmdline += " -mcache-lookup-bc"
@@ -9,7 +9,7 @@ def compile(file, cfile=None):
         cmdline += " -o %s" % cfile
     res = os.system(cmdline)
     if res:
-        raise OSError("Error running mpy-cross, rc=%d" % (res >> 8))
+        raise OSError("Error running pycopy-cross, rc=%d" % (res >> 8))
 
 if __name__ == "__main__":
     import sys
