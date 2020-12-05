eca14eb32221c389edc7d69d621d9d93af6077c9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-12-05 07:28:01 +0300

    ast: testdata/comprehensions_async: Testcase for async comprehension.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ ast/testdata/comprehensions_async.py
@@ -0,0 +1,22 @@
+async def foo():
+    {x: x + 1 async for x in y}
+
+    (x async for x in foo)
+    (x for x in foo if bar)
+    (x for x in foo if bar if baz)
+    any(x for x in foo)
+    [x for x in foo]
+    [(x for x in foo)]
+    [x for x in foo if 2]
+
+    (x for x in y for y in foo)
+    (x for x in y for y in foo if y)
+    (x for x in y if x async for y in foo)
+    (x for x in y if x async for y in foo if y)
+    [x async for x in y if x for y in foo if y]
+
+    {x for x in y}
+
+    [lambda: None for x in y]
+
+    [x for x in s].copy()
