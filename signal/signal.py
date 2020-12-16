63119d414df82947affc17c9099e24893fa64823
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-12-17 00:32:21 +0300

    signal: Store FFI callbacks for Python handlers in top-level dict.
    To preclude them from being garbage-collected and segfaults which will
    follow.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- signal/signal.py
+++ signal/signal.py
@@ -14,9 +14,17 @@ libc = ffilib.libc()
 signal_i = libc.func("i", "signal", "ii")
 signal_p = libc.func("i", "signal", "ip")
 
+_hmap = {}
+
+
 def signal(n, handler):
     if isinstance(handler, int):
+        # We don't try to remove callback from _hmap here, as we return old
+        # signal handler. If it's based on callback installed earlier, it
+        # can be reinstated again via next call to signal(), so we still
+        # need to keep the callback around.
         return signal_i(n, handler)
     import ffi
     cb = ffi.callback("v", handler, "i")
+    _hmap[n] = cb
     return signal_p(n, cb)
