70501137382526b11f19f6521dd413aed9c30d9c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-01-05 00:52:14 +0300

    ast: testdata: import_from: Add testcase for multiple dots in relative imports.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- ast/testdata/import_from.py
+++ ast/testdata/import_from.py
@@ -1,8 +1,13 @@
 from . import foo
 from .. import foo
+from ... import foo
+from .... import foo
 from bar import foo
 from .bar import foo
+from ...bar import foo
+from ....bar import foo
 from bar.baz import foo
+from ....bar.baz import foo
 from . import foo as foo1, bar, baz as baz2
 
 from bar import (one, two)
