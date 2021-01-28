687fc7d188335818450a67aad203ffa02984ff93
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-01-29 00:40:31 +0300

    os: Update copyright year.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- os/os/__init__.py
+++ os/os/__init__.py
@@ -6,7 +6,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2014-2019 Paul Sokolovsky
+# Copyright (c) 2014-2021 Paul Sokolovsky
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
