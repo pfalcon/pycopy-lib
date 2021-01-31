ac3d12843373b4460315acfa6c31de6de3991191
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-01-31 23:51:58 +0300

    uos2: Split baseline POSIX functions from "os".
    "os" is intended to be compatile with CPython, and thus ending up to contain
    various baggage not needed for a minimalist implementation, and will grow
    only more of that over time. So, split baseline stuff to "uos2", and let
    "os" build on top of that.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- os/os/__init__.py
+++ os/os/__init__.py
@@ -30,25 +30,12 @@ import uarray as array
 import ustruct as struct
 import errno as errno_
 import stat as stat_
-import ffilib
 import uos
 from . import path
 
-R_OK = const(4)
-W_OK = const(2)
-X_OK = const(1)
-F_OK = const(0)
+from uos2 import *
+from uos2 import _exit, _libc as libc
 
-O_ACCMODE  = const(0o0000003)
-O_RDONLY   = const(0o0000000)
-O_WRONLY   = const(0o0000001)
-O_RDWR     = const(0o0000002)
-O_CREAT    = const(0o0000100)
-O_EXCL     = const(0o0000200)
-O_NOCTTY   = const(0o0000400)
-O_TRUNC    = const(0o0001000)
-O_APPEND   = const(0o0002000)
-O_NONBLOCK = const(0o0004000)
 
 P_WAIT = 0
 P_NOWAIT = 1
@@ -61,46 +48,11 @@ pardir = const("..")
 linesep = const("\n")
 
 
-libc = ffilib.libc()
-
 if libc:
-    chdir_ = libc.func("i", "chdir", "s")
-    mkdir_ = libc.func("i", "mkdir", "si")
-    rename_ = libc.func("i", "rename", "ss")
-    unlink_ = libc.func("i", "unlink", "s")
-    rmdir_ = libc.func("i", "rmdir", "s")
-    getcwd_ = libc.func("s", "getcwd", "si")
     opendir_ = libc.func("P", "opendir", "s")
     readdir_ = libc.func("P", "readdir", "P")
-    open_ = libc.func("i", "open", "sii")
-    read_ = libc.func("i", "read", "ipi")
-    write_ = libc.func("i", "write", "iPi")
-    lseek_ = libc.func("l", "lseek", "ili")
-    close_ = libc.func("i", "close", "i")
     dup_ = libc.func("i", "dup", "i")
-    access_ = libc.func("i", "access", "si")
-    fork_ = libc.func("i", "fork", "")
-    pipe_ = libc.func("i", "pipe", "p")
-    _exit_ = libc.func("v", "_exit", "i")
-    getpid_ = libc.func("i", "getpid", "")
-    waitpid_ = libc.func("i", "waitpid", "ipi")
-    system_ = libc.func("i", "system", "s")
-    execvp_ = libc.func("i", "execvp", "PP")
-    kill_ = libc.func("i", "kill", "ii")
-    getenv_ = libc.func("s", "getenv", "P")
-    putenv_ = libc.func("i", "putenv", "s")
-    utimes_ = libc.func("i", "utimes", "sP")
 
-
-
-def check_error(ret):
-    # Return True is error was EINTR (which usually means that OS call
-    # should be restarted).
-    if ret == -1:
-        e = uos.errno()
-        if e == errno_.EINTR:
-            return True
-        raise OSError(e)
 
 def raise_error():
     raise OSError(uos.errno())
@@ -126,39 +78,6 @@ def stat(name):
 def lstat(name):
     return stat_result(uos.stat(name, False))
 
-
-if hasattr(uos, "getcwd"):
-    getcwd = uos.getcwd
-else:
-    def getcwd():
-        buf = bytearray(512)
-        return getcwd_(buf, 512)
-
-def mkdir(name, mode=0o777):
-    e = mkdir_(name, mode)
-    check_error(e)
-
-if hasattr(uos, "rename"):
-    rename = uos.rename
-else:
-    def rename(old, new):
-        e = rename_(old, new)
-        check_error(e)
-
-if hasattr(uos, "remove"):
-    unlink = remove = uos.remove
-else:
-    def unlink(name):
-        e = unlink_(name)
-        check_error(e)
-    remove = unlink
-
-if hasattr(uos, "rmdir"):
-    rmdir = uos.rmdir
-else:
-    def rmdir(name):
-        e = rmdir_(name)
-        check_error(e)
 
 def makedirs(name, mode=0o777, exist_ok=False):
     s = ""
@@ -229,106 +148,12 @@ def walk(top, topdown=True):
     if not topdown:
         yield top, dirs, files
 
-def open(n, flags, mode=0o777):
-    r = open_(n, flags, mode)
-    check_error(r)
-    return r
-
-def read(fd, n):
-    buf = bytearray(n)
-    r = read_(fd, buf, n)
-    check_error(r)
-    return bytes(buf[:r])
-
-def write(fd, buf):
-    r = write_(fd, buf, len(buf))
-    check_error(r)
-    return r
-
-def lseek(fd, pos, how):
-    r = lseek_(fd, pos, how)
-    check_error(r)
-    return r
-
-def close(fd):
-    r = close_(fd)
-    check_error(r)
-    return r
 
 def dup(fd):
     r = dup_(fd)
     check_error(r)
     return r
 
-def access(path, mode):
-    return access_(path, mode) == 0
-
-def utime(path, times):
-    at_s = int(times[0])
-    mt_s = int(times[1])
-    at_us = int((times[0] - at_s) * 1000000)
-    mt_us = int((times[1] - mt_s) * 1000000)
-    arr = array.array("L", [at_s, at_us, mt_s, mt_us])
-    r = utimes_(path, arr)
-    check_error(r)
-
-if hasattr(uos, "chdir"):
-    chdir = uos.chdir
-else:
-    def chdir(dir):
-        r = chdir_(dir)
-        check_error(r)
-
-def fork():
-    r = fork_()
-    check_error(r)
-    return r
-
-def pipe():
-    a = array.array('i', [0, 0])
-    r = pipe_(a)
-    check_error(r)
-    return a[0], a[1]
-
-def _exit(n):
-    _exit_(n)
-
-def execvp(f, args):
-    import uctypes
-    args_ = array.array("P", [0] * (len(args) + 1))
-    i = 0
-    for a in args:
-        args_[i] = uctypes.addressof(a)
-        i += 1
-    r = execvp_(f, uctypes.addressof(args_))
-    check_error(r)
-
-def getpid():
-    return getpid_()
-
-def waitpid(pid, opts):
-    a = array.array('i', [0])
-    r = waitpid_(pid, a, opts)
-    check_error(r)
-    return (r, a[0])
-
-def kill(pid, sig):
-    r = kill_(pid, sig)
-    check_error(r)
-
-def system(command):
-    r = system_(command)
-    check_error(r)
-    return r
-
-def getenv(var, default=None):
-    var = getenv_(var)
-    if var is None:
-        return default
-    return var
-
-def putenv(key, value):
-    return putenv_(key + "=" + value)
 
 def fsencode(s):
     if type(s) is bytes:
---
+++ uos2/uos2.py
@@ -0,0 +1,243 @@
+# uos2 - more POSIX OS functions than builtin "uos" module, but no additional
+# baggage as in CPython's "os".
+#
+# This file is part of the standard library of Pycopy project, minimalist
+# and lightweight Python implementation.
+#
+# https://github.com/pfalcon/pycopy
+# https://github.com/pfalcon/pycopy-lib
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014-2021 Paul Sokolovsky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import uos
+import uarray
+import errno
+import ffilib
+
+
+R_OK = const(4)
+W_OK = const(2)
+X_OK = const(1)
+F_OK = const(0)
+
+O_ACCMODE  = const(0o0000003)
+O_RDONLY   = const(0o0000000)
+O_WRONLY   = const(0o0000001)
+O_RDWR     = const(0o0000002)
+O_CREAT    = const(0o0000100)
+O_EXCL     = const(0o0000200)
+O_NOCTTY   = const(0o0000400)
+O_TRUNC    = const(0o0001000)
+O_APPEND   = const(0o0002000)
+O_NONBLOCK = const(0o0004000)
+
+
+_libc = ffilib.libc()
+
+_getcwd = _libc.func("s", "getcwd", "si")
+_chdir = _libc.func("i", "chdir", "s")
+_mkdir = _libc.func("i", "mkdir", "si")
+_rmdir = _libc.func("i", "rmdir", "s")
+_rename = _libc.func("i", "rename", "ss")
+_unlink = _libc.func("i", "unlink", "s")
+_access = _libc.func("i", "access", "si")
+_utimes = _libc.func("i", "utimes", "sP")
+
+_open = _libc.func("i", "open", "sii")
+_read = _libc.func("i", "read", "ipi")
+_write = _libc.func("i", "write", "iPi")
+_lseek = _libc.func("l", "lseek", "ili")
+_close = _libc.func("i", "close", "i")
+
+_dup2 = _libc.func("i", "dup2", "ii")
+_pipe = _libc.func("i", "pipe", "p")
+
+getpid = _libc.func("i", "getpid", "")  # no wrapper (nw)
+_exit = _libc.func("v", "_exit", "i")  # nw
+_kill = _libc.func("i", "kill", "ii")
+_fork = _libc.func("i", "fork", "")
+_waitpid = _libc.func("i", "waitpid", "ipi")
+_execvp = _libc.func("i", "execvp", "PP")
+_system = _libc.func("i", "system", "s")
+
+_getenv = _libc.func("s", "getenv", "P")
+_putenv = _libc.func("i", "putenv", "s")
+
+
+def check_error(ret):
+    # Return True is error was EINTR (which usually means that OS call
+    # should be restarted).
+    if ret == -1:
+        e = uos.errno()
+        if e == errno.EINTR:
+            return True
+        raise OSError(e)
+
+
+if hasattr(uos, "getcwd"):
+    getcwd = uos.getcwd
+else:
+    def getcwd():
+        buf = bytearray(512)
+        return _getcwd(buf, 512)
+
+
+if hasattr(uos, "chdir"):
+    chdir = uos.chdir
+else:
+    def chdir(dir):
+        r = _chdir(dir)
+        check_error(r)
+
+
+def mkdir(name, mode=0o777):
+    e = _mkdir(name, mode)
+    check_error(e)
+
+
+if hasattr(uos, "rmdir"):
+    rmdir = uos.rmdir
+else:
+    def rmdir(name):
+        e = _rmdir(name)
+        check_error(e)
+
+
+if hasattr(uos, "rename"):
+    rename = uos.rename
+else:
+    def rename(old, new):
+        e = _rename(old, new)
+        check_error(e)
+
+if hasattr(uos, "remove"):
+    unlink = remove = uos.remove
+else:
+    def unlink(name):
+        e = _unlink(name)
+        check_error(e)
+    remove = unlink
+
+
+def access(path, mode):
+    return _access(path, mode) == 0
+
+
+def utime(path, times):
+    at_s = int(times[0])
+    mt_s = int(times[1])
+    at_us = int((times[0] - at_s) * 1000000)
+    mt_us = int((times[1] - mt_s) * 1000000)
+    arr = uarray.array("L", [at_s, at_us, mt_s, mt_us])
+    r = _utimes(path, arr)
+    check_error(r)
+
+
+def open(n, flags, mode=0o777):
+    r = _open(n, flags, mode)
+    check_error(r)
+    return r
+
+
+def read(fd, n):
+    buf = bytearray(n)
+    r = _read(fd, buf, n)
+    check_error(r)
+    return bytes(buf[:r])
+
+
+def write(fd, buf):
+    r = _write(fd, buf, len(buf))
+    check_error(r)
+    return r
+
+
+def lseek(fd, pos, how):
+    r = _lseek(fd, pos, how)
+    check_error(r)
+    return r
+
+
+def close(fd):
+    r = _close(fd)
+    check_error(r)
+    return r
+
+
+def dup2(o, n):
+    r = _dup2(o, n)
+    check_error(r)
+    return r
+
+
+def pipe():
+    a = uarray.array("i", [0, 0])
+    r = _pipe(a)
+    check_error(r)
+    return a[0], a[1]
+
+
+def kill(pid, sig):
+    r = _kill(pid, sig)
+    check_error(r)
+
+
+def fork():
+    r = _fork()
+    check_error(r)
+    return r
+
+
+def waitpid(pid, opts):
+    a = uarray.array("i", [0])
+    r = _waitpid(pid, a, opts)
+    check_error(r)
+    return r, a[0]
+
+
+def execvp(f, args):
+    import uctypes
+    args_ = uarray.array("P", [0] * (len(args) + 1))
+    i = 0
+    for a in args:
+        args_[i] = uctypes.addressof(a)
+        i += 1
+    r = _execvp(f, args_)
+    check_error(r)
+
+
+def system(c):
+    r = _system(c)
+    check_error(r)
+    return r
+
+
+def getenv(var, default=None):
+    var = _getenv(var)
+    if var is None:
+        return default
+    return var
+
+
+def putenv(key, value):
+    return _putenv(key + "=" + value)
