15c9fb775053cfe707d0e286fd0553617423ebc4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-02-17 23:04:44 +0300

    warnings: Warnings should go to stderr.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- warnings/warnings.py
+++ warnings/warnings.py
@@ -1,2 +1,5 @@
+import sys
+
+
 def warn(msg, cat=None, stacklevel=1):
-    print("%s: %s" % ("Warning" if cat is None else cat.__name__, msg))
+    print("%s: %s" % ("Warning" if cat is None else cat.__name__, msg), file=sys.stderr)
