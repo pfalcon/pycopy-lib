18a456845d86bf664f6f32a04fca0ec4ebd202c5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-02-24 11:08:15 +0300

    tempfile: Add simple TemporaryFile implementation.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tempfile/tempfile.py
+++ tempfile/tempfile.py
@@ -1,5 +1,13 @@
 import utime
+from uos2 import remove
 
 
 def mktemp():
     return "/tmp/tmp%d" % utime.time()
+
+
+def TemporaryFile(mode="w+b"):
+    n = mktemp()
+    f = open(n, mode)
+    remove(n)
+    return f
