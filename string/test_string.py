c88f6f617fba795d26de54d6546919f9b8924ef7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-04-02 00:15:44 +0300

    string: test_string: Add testcases for isalnum().
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- string/test_string.py
+++ string/test_string.py
@@ -2,6 +2,12 @@ from string import *
 
 assert translate("foobar", {ord("o"): "foo", ord("b"): 32, ord("r"): None}) == "ffoofoo a"
 
+assert isalnum("898dsdsd")
+assert isalnum("sdsd64645")
+assert not isalnum("")
+assert not isalnum("sdsd_64645")
+assert not isalnum("<EOF>")
+
 assert isidentifier("_foo13_1")
 assert not isidentifier("_foo13_1-")
 assert not isidentifier("1_foo13_1")
