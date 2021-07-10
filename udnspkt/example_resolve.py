3c6415f8f42b93848d2188ae7f933dcbbfcd5be4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-07-10 22:35:35 +0300

    udnspkt: example_resolve: Elaborate example.
    Use 8.8.8.8 instead of localhost as the DNS server, accept host to resolve
    as commandline arg, improve printing of results.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- udnspkt/example_resolve.py
+++ udnspkt/example_resolve.py
@@ -1,3 +1,4 @@
+import sys
 import uio
 import usocket
 
@@ -5,25 +6,32 @@ import udnspkt
 
 
 s = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
-dns_addr = usocket.getaddrinfo("127.0.0.1", 53)[0][-1]
+dns_addr = usocket.getaddrinfo("8.8.8.8", 53)[0][-1]
 
 
 def resolve(domain, is_ipv6):
     buf = uio.BytesIO(48)
-    udnspkt.make_req(buf, "google.com", is_ipv6)
+    udnspkt.make_req(buf, domain, is_ipv6)
     v = buf.getvalue()
-    print("query: ", v)
+    print("# query: ", v)
     s.sendto(v, dns_addr)
 
     resp = s.recv(1024)
-    print("resp:", resp)
+    print("# resp:", resp)
     buf = uio.BytesIO(resp)
 
     addr = udnspkt.parse_resp(buf, is_ipv6)
-    print("bin addr:", addr)
-    print("addr:", usocket.inet_ntop(usocket.AF_INET6 if is_ipv6 else usocket.AF_INET, addr))
+    print("# bin addr:", addr)
+    if addr:
+        print("addr:", usocket.inet_ntop(usocket.AF_INET6 if is_ipv6 else usocket.AF_INET, addr))
 
 
-resolve("google.com", False)
+host = "google.com"
+if len(sys.argv) > 1:
+    host = sys.argv[1]
+
+print(host, "IPv4")
+resolve(host, False)
 print()
-resolve("google.com", True)
+print(host, "IPv6")
+resolve(host, True)
