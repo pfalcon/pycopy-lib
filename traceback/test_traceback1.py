4bb8089308c0334eadc3ddcd07ffea560632bab8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-07-23 10:53:06 +0300

    traceback: test_traceback1: Add test for fully qualified exc class name.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- traceback/test_traceback1.py
+++ traceback/test_traceback1.py
@@ -8,3 +8,16 @@ def f(e):
 assert f(ValueError()) == ["ValueError\n"]
 assert f(ValueError(1)) == ["ValueError: 1\n"]
 assert f(ValueError(1, 2)) == ["ValueError: (1, 2)\n"]
+
+
+class MyExc(Exception):
+    pass
+
+
+assert f(MyExc()) == ["MyExc\n"]
+
+
+from test_traceback2 import MyExc2
+
+
+assert f(MyExc2()) == ["test_traceback2.MyExc2\n"]
