9aedea66ea2020e23cb556164f1b619278294018
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-09-16 21:24:27 +0300

    selectors: example_stdin.py: More verbose output.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- selectors/example_stdin.py
+++ selectors/example_stdin.py
@@ -4,8 +4,8 @@ from selectors import *
 f = open(0, "rb")
 
 p = DefaultSelector()
-print(p)
-print(p.register(f, EVENT_READ, "userdata"))
+print("DefaultSelector():", p)
+print("register:", p.register(f, EVENT_READ, "userdata"))
 
 for r in p.select():
     print(r)
