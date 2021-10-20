d3af716be0c3089c2eacecdd0ff482da89d01d64
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-10-20 14:51:26 +0300

    os.path: Add commonprefix().
    From CPython git tag v3.4.10.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- os.path/os/path.py
+++ os.path/os/path.py
@@ -114,3 +114,16 @@ def expanduser(s):
         # Sorry folks, follow conventions
         return "/home/" + s[1:]
     return s
+
+
+# From CPython git tag v3.4.10.
+# Return the longest prefix of all list elements.
+def commonprefix(m):
+    "Given a list of pathnames, returns the longest common leading component"
+    if not m: return ''
+    s1 = min(m)
+    s2 = max(m)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i]
+    return s1
