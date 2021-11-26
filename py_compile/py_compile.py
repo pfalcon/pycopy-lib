00e41642c5a72e2203233c393e9a15604b31cf73
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-11-26 21:45:47 +0300

    py_compile: Set mtime of the generated .mpy to the same value as original .py.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- py_compile/py_compile.py
+++ py_compile/py_compile.py
@@ -7,9 +7,13 @@ def compile(file, cfile=None):
     cmdline += " -mcache-lookup-bc"
     if cfile is not None:
         cmdline += " -o %s" % cfile
+    else:
+        cfile = file.rsplit(".", 1)[0] + ".mpy"
     res = os.system(cmdline)
     if res:
         raise OSError("Error running pycopy-cross, rc=%d" % (res >> 8))
+    mtime = os.stat(file)[8]
+    os.utime(cfile, (mtime, mtime))
 
 if __name__ == "__main__":
     import sys
