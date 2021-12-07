0a7588b26aa1510b128c1cf090c292441966657c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-12-07 19:04:31 +0300

    collections.chainmap: test_chainmap: Add test for .items().
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- collections.chainmap/test_chainmap.py
+++ collections.chainmap/test_chainmap.py
@@ -6,6 +6,9 @@ assert cm["k1"] == "v11"
 assert cm["k2"] == "v2"
 assert cm["k3"] == "v3"
 
+items = sorted(list(cm.items()))
+assert items == [('k1', 'v11'), ('k2', 'v2'), ('k3', 'v3')]
+
 del cm["k1"]
 assert cm["k1"] == "v12"
 cm["k1"] = "newv"
